7e8a5e693aca8390acdbf8ee9c54dcc40dce6c82
Geoffrey Gaillard <geoffrey@MacBook-Pro-de-Geoffrey.local>
2019-03-11 03:29:42 +0100

    Basic wrapper around java api

--- src/webpush/core.clj
+++ src/webpush/core.clj
@@ -1,6 +1,40 @@
-(ns webpush.core)
+(ns webpush.core
+  (:import java.security.Security
+           (org.bouncycastle.jce.provider BouncyCastleProvider)
+           (nl.martijndwars.webpush Notification PushService
+                                    Subscription Subscription$Keys)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn add-security-provider! []
+  (Security/addProvider (BouncyCastleProvider.)))
+
+(defn subscription [subscription-map]
+  (let [endpoint           (:endpoint subscription-map)
+        {:keys [key auth]} (:keys subscription-map)
+        keys               (Subscription$Keys. key auth)]
+    (Subscription. endpoint keys)))
+
+(defn push-service
+  ([gcm-api-key]
+   (PushService. gcm-api-key))
+  ([key-pair subject]
+   (PushService. key-pair subject))
+  ([public-key private-key subject]
+   (PushService. public-key private-key subject)))
+
+(defn notification [subscription payload]
+  (Notification. subscription payload))
+
+(defn send [push-service notification]
+  (.send push-service notification))
+
+(defn send-async [push-service notification]
+  (.sendAsync push-service notification))
+
+(comment
+  (add-security-provider!)
+  (let [sub   (subscription {:endpoint "https://random-push-service.com/some-kind-of-unique-id-1234/v2/"
+                             :keys     {:key  "BNcRdreALRFXTkOOUHK1EtK2wtaz5Ry4YfYCA_0QTpQtUbVlUls0VJXg7A8u-Ts1XbjhazAkj7I99e8QcYP7DkM="
+                                        :auth "tBHItJI5svbpez7KI4CCXg=="}})
+        svc   (push-service)
+        notif (notification sub "hello")]
+    (send push-service notif)))
