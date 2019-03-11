ef62404fa262856941c8066f612a95afb1368a5c
Geoffrey Gaillard <geoffrey@MacBook-Pro-de-Geoffrey.local>
2019-03-11 03:29:22 +0100

    Add BouncyCastle dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "MIT"
             :url  "https://github.com/ggeoffrey/webpush-clojure/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [nl.martijndwars/web-push "5.0.1"]]
+                 [nl.martijndwars/web-push "5.0.1"]
+                 [org.bouncycastle/bcprov-jdk15on "1.61"]]
   :repl-options {:init-ns webpush.core})
